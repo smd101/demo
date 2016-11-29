b537d263e8e6ec357f8a57ae71bc790376cf6ff1
seiji.shimada <s.shimada@tlega.co.jp>
2016-11-29 10:50:31 +0900

    mod stream to map を追加.

--- src/test/java/com/example/JavaTest.java
+++ src/test/java/com/example/JavaTest.java
@@ -2,27 +2,66 @@ package com.example;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import lombok.Value;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JavaTest {
 
-	@Test
-	public void OptionalTest() {
-	  Optional<String> notNullOpt = Optional.of("aaa");
-	  String actual1 = notNullOpt.map(v->v).orElse(null);
-	  assertThat(actual1).isEqualTo("aaa");
+  /**
+   * Optional の isPresent により、取得値を切り分ける.
+   */
+  @Test
+  public void howto_optional_return() {
+    Optional<String> notNullOpt = Optional.of("aaa");
+    String actual1 = notNullOpt.map(v->v).orElse(null);
+    assertThat(actual1).isEqualTo("aaa");
 
-	  Optional<String> nullOpt = Optional.empty();
+    Optional<String> nullOpt = Optional.empty();
     String actual2 = nullOpt.map(v->v).orElse(null);
     assertThat(actual2).isNull();
 
-	}
+  }
+
+  
+  @Value
+  private static class Hoge {
+    private String key;
+    private String value;
+  }
+
+  /**
+   * stream の値を元に、Map を作成する.
+   */
+  @Test
+  public void howto_stream_to_map() {
+    List<Hoge> hoges = new ArrayList<>();
+    hoges.add(new Hoge("key1", "value1"));
+    hoges.add(new Hoge("key2", "value2"));
+    hoges.add(new Hoge("key3", "value3"));
+    
+    Map<String, Hoge> dictionary = hoges.stream()
+        .filter(v -> v.key.equals("key1")
+            || v.key.equals("key2"))
+        .collect(Collectors.toMap(Hoge::getKey, v->v));
+
+    assertThat(dictionary.containsKey("key1")).isTrue();
+    assertThat(dictionary.containsKey("key2")).isTrue();
+    assertThat(dictionary.containsKey("key3")).isFalse();
+
+    assertThat(dictionary.get("key1").getValue()).isEqualTo("value1");
+
+  }
 
 }
